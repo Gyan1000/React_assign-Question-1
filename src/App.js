89a161580c829bab8ad359870d2391f37d9d1c75
unknown <gyanjha1000@gmail.com>
2023-09-02 00:13:16 +0530

    react-assignment-question-1

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import  Person from './Person'
+function App() {
+  return (
+    
+    <div className="App">
+      <header className="App-header">
+        <Person name="Shri_Ram" age="55"/>
+      </header>
+    </div>
+    
+  );
+}
+
+export default App;
